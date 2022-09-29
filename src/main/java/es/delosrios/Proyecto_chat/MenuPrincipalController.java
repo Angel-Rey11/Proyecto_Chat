f359a94a507d9136a30dcaca4099c29b608af3c4
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2022-09-29 13:40:42 +0200

    Avanzamos

--- src/main/java/es/delosrios/Proyecto_chat/MenuPrincipalController.java
+++ src/main/java/es/delosrios/Proyecto_chat/MenuPrincipalController.java
@@ -2,6 +2,9 @@ package es.delosrios.Proyecto_chat;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.DialogPane;
 import javafx.scene.image.ImageView;
 
 public class MenuPrincipalController {

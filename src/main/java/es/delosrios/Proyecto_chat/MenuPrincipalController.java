09c947d3a02b07cd7bf2989af1d86574444437e1
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2022-09-29 12:56:08 +0200

    Cambios

--- src/main/java/es/delosrios/Proyecto_chat/MenuPrincipalController.java
+++ src/main/java/es/delosrios/Proyecto_chat/MenuPrincipalController.java
@@ -2,11 +2,14 @@ package es.delosrios.Proyecto_chat;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 
 public class MenuPrincipalController {
+	
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+    
 }

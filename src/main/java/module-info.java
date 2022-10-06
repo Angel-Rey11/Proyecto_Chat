040e93f2c5f05dac9252da4fcf346eba3da2c759
Franpd25 <franprados025@gmail.com>
2022-10-06 17:06:18 +0200

    Diseñar clases de utils

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,12 +2,15 @@ module es.delosrios.Proyecto_chat {
     requires javafx.controls;
     requires javafx.fxml;
 	requires java.xml.bind;
+	requires java.xml;
 	requires javafx.base;
 	requires javafx.graphics;
+	requires java.logging;
 
 
     opens es.delosrios.Proyecto_chat to javafx.fxml,java.xml.bind;
     opens es.delosrios.Proyecto_chat.model.Dao to java.xml.bind,javafx.fxml;
     opens es.delosrios.Proyecto_chat.model.DataObject;
+    opens es.delosrios.Proyecto_chat.utils to java.xml.bind;
     exports es.delosrios.Proyecto_chat;
 }

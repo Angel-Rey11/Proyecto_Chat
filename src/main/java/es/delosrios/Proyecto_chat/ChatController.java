acf76bcd56de73117890e530736c01e334274cb4
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2022-10-07 14:16:28 +0200

    Avanzamos

--- src/main/java/es/delosrios/Proyecto_chat/ChatController.java
+++ src/main/java/es/delosrios/Proyecto_chat/ChatController.java
@@ -3,6 +3,9 @@ package es.delosrios.Proyecto_chat;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import es.delosrios.Proyecto_chat.model.Dao.RepoSala;
+import es.delosrios.Proyecto_chat.model.DataObject.Sala;
 import es.delosrios.Proyecto_chat.utils.DataService;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -20,16 +23,28 @@ public class ChatController implements Initializable{
 	@FXML
 	private Label nUsuarios;
 	
+	RepoSala s = new RepoSala();
+	
 	@FXML
     private void switchToMenuPrincipal()  throws IOException {
+		s.unmarshall("Salas.xml");
     	App.setRoot("MenuPrincipal");
     }
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		s.unmarshall("Salas.xml");
 		user.setText(DataService.user.getNickName());
 		nombreS.setText(DataService.sala.getNombre());
 		nUsuarios.setText(String.valueOf(DataService.sala.getAllUsers().size()));
 		
+	}
+	
+	@FXML
+	public void outSala() throws IOException {
+		DataService.sala.removeUser(DataService.user);
+		s.searchSala(DataService.sala);
+		
+		App.setRoot("Salas");
 	}
 }

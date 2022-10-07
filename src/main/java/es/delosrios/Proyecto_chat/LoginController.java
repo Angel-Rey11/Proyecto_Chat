31459dd8e5ee5317b804452deb723e9376c0d83e
franciscoberral <fberzaf1908@g.educaand.es>
2022-10-07 14:27:44 +0200

    Modificado

--- src/main/java/es/delosrios/Proyecto_chat/LoginController.java
+++ src/main/java/es/delosrios/Proyecto_chat/LoginController.java
@@ -8,6 +8,8 @@ import es.delosrios.Proyecto_chat.model.Dao.RepoSala;
 import es.delosrios.Proyecto_chat.model.Dao.RepoUsuario;
 import es.delosrios.Proyecto_chat.model.DataObject.Usuario;
 import es.delosrios.Proyecto_chat.utils.DataService;
+import es.delosrios.Proyecto_chat.utils.Dialog;
+import es.delosrios.Proyecto_chat.utils.Loggers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
@@ -52,23 +54,35 @@ public class LoginController implements Initializable{
 		String nickname = nicknameInicio.getText();
 		String password = passwordInicio.getText();
 		Usuario aux = ru.validateLogin(nickname, password);
-		if (aux == null) {
-			System.out.println("No");
+		if (nickname.isEmpty() || password.isEmpty()) {
+			Dialog.showError("ERROR", "FALLO AL INTRODUCIR DATOS", "TODOS LOS CAMPOS DEBEN SER COMPLETADOS");
+			Loggers.LogsSevere("TODOS LOS CAMPOS DEBEN SER COMPLETADOS");
 		} else {
-			DataService.user=aux;
-			inicio.setVisible(false);
-			App.setRoot("MenuPrincipal");
+			if (aux == null) {
+				Dialog.showError("ERROR", "USUARIO NO ENCONTRADO", "EL USUARIO INTRODUCIDO NO EXISTE");
+				Loggers.LogsSevere("EL USUARIO INTRODUCIDO NO ES VÁLIDO");
+			} else {
+				DataService.user=aux;
+				inicio.setVisible(false);
+				Dialog.showConfirm("OPERACIÓN EXITOSA", "ACCESO PERMITIDO", "BIENVENIDO");
+				App.setRoot("MenuPrincipal");
+			}
 		}
-		
 	}
 	
 	@FXML
 	private void signUp() {
-		Usuario u = new Usuario(nombreUser.getText(),passwordUser.getText(),nicknameUser.getText());
-		ru.addUsuario(u);
-		ru.marshall("Usuarios.xml");
-		registro.setVisible(false);
-		inicio.setVisible(true);
+		if (!nombreUser.getText().isEmpty() && !passwordUser.getText().isEmpty() && !nicknameUser.getText().isEmpty()) {
+			Usuario u = new Usuario(nombreUser.getText(),passwordUser.getText(),nicknameUser.getText());
+			ru.addUsuario(u);
+			ru.marshall("Usuarios.xml");
+			Dialog.showConfirm("OPERACIÓN EXITOSA", "USUARIO CREADO", "SE HA CREADO EL NUEVO USUARIO CORRECTAMENTE");
+			registro.setVisible(false);
+			inicio.setVisible(true);
+		} else {
+			Dialog.showError("ERROR", "FALLO AL CREAR USUARIO", "TODOS LOS CAMPOS DEBEN SER COMPLETADOS");
+			Loggers.LogsSevere("FALTAN CAMPOS POR RELLENAR");
+		}
 	}
 
 	@Override
